a448959c4c43d81a1138bbf30646a680e6c9fbd4
ssn <ssnfee@gmail.com>
2016-04-15 14:12:11 +0800

    Initial Commit

---
+++ CollectionViewStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewStoryboard
+//
+//  Created by sseen on 16/4/15.
+//  Copyright © 2016年 ssn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewStoryboard
+//
+//  Created by sseen on 16/4/15.
+//  Copyright © 2016年 ssn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
